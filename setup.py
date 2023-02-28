7a039ab2f7126519f4f827115323aeb6dec6bcbf
Pieter Roggemans <pieter.roggemans@gmail.com>
2023-02-28 10:54:57 +0100

    Move config info from setup.py to pyproject.toml (#8)

--- setup.py
+++ setup.py
@@ -1,28 +1,9 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 with open("pygeoops/version.txt", mode="r") as file:
     version = file.readline()
 
 setuptools.setup(
     name="pygeoops",
     version=version,
-    author="Pieter Roggemans",
-    author_email="pieter.roggemans@gmail.com",
-    description="PyGeoOps provides some less common or extended spatial algorithms and utility functions.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/pygeoops/pygeoops",
-    include_package_data=True,
-    packages=setuptools.find_packages(),
-    install_requires=[
-        "shapely>1",
-    ],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.8",
 )
