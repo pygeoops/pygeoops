edcf1aced164d508d2a57d725943de0a001a7b30
Pieter Roggemans <pieter.roggemans@gmail.com>
2023-08-20 09:27:08 +0200

    Add normalize to assert in test_difference_None_empty

--- tests/test_shapely.py
+++ tests/test_shapely.py
@@ -6,6 +6,7 @@ Some tests mainly meant to explore/show behaviour of some shapely functions.
 import numpy as np
 import pytest
 import shapely
+from shapely.testing import assert_geometries_equal
 
 
 def test_difference():
@@ -77,4 +78,6 @@ def test_difference_None_empty():
     assert shapely.difference(poly, None) is None
 
     # Subtract with empty gives the original poly
-    assert shapely.difference(poly, shapely.Polygon()) == poly
+    assert_geometries_equal(
+        shapely.difference(poly, shapely.Polygon()), poly, normalize=False
+    )
