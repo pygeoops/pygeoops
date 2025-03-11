3d6d6ba8c04985e06eb82b6918c17c302e67f57d
Pieter Roggemans <pieter.roggemans@gmail.com>
2025-03-11 09:36:32 +0100

    Remove left sidebars

--- docs/conf.py
+++ docs/conf.py
@@ -91,6 +91,14 @@ html_theme_options = {
     ]
 }
 
+html_sidebars: dict[str, list[str]] = {
+    "development": [],
+    "faq": [],
+    "installation": [],
+    "reference": [],
+    "user_guide": [],
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
