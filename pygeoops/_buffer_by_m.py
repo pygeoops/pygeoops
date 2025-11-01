6baec4b982ed0d1e40ba6bc6d9b04eb942bfcd1e
Pieter Roggemans <pieter.roggemans@gmail.com>
2025-11-01 18:13:54 +0100

    Special treatment of 0 distance m values

--- pygeoops/_buffer_by_m.py
+++ pygeoops/_buffer_by_m.py
@@ -30,11 +30,14 @@ def buffer_by_m(
     Calculates a variable width buffer for a geometry.
 
     The buffer distance at each vertex is determined by the M value of that vertex, or
-    the Z value if M is not available.
+    the Z value if M is not available. If a distance is zero, the resulting buffer will
+    taper towards the original point and the result will be a multipolygon where the
+    parts touch at that point. If a distance is negative, the resulting buffer will
+    omit that point and the result will be a multipolygon with disjoint parts.
 
-    Example output with default parameters (grey: original polygon, blue: buffer):
+    Example output (grey: original polygon, blue: buffer):
 
-    .. plot:: code/buffer_by_m_basic.py
+    .. plot:: code/buffer_by_m_different_cases.py
 
     Alternative name: variable width buffer.
 
@@ -54,19 +57,27 @@ def buffer_by_m(
             geometries.
 
     Example:
-        These are some more visualisation of buffers calculated with some different
-        options specified.
+    .. code-block:: python
 
-        .. plot:: code/buffer_by_m_options.py
-
+        line = LineString([[0, 6, 1], [0, 0, 2], [10, 0, 2], [13, 5, 4]])
+        buffer_geom = buffer_by_m(line)
 
     """
     if line is None:
         return None
     line = _extract_0dim_ndarray(line)
 
-    # If input is arraylike, treat every geometry in loop
-    if hasattr(line, "__len__"):
+    # If input is not arraylike, treat the single geometry
+    if not hasattr(line, "__len__"):
+        return _buffer_by_m(
+            line=line,
+            quad_segs=quad_segs,
+            cap_style=cap_style,
+            join_style=join_style,
+            mitre_limit=mitre_limit,
+        )
+    else:
+        # Arraylike, so treat every geometry in loop
         result = np.array(
             [
                 _buffer_by_m(
@@ -83,15 +94,6 @@ def buffer_by_m(
             result = GeoSeries(result, index=line.index, crs=line.crs)
 
         return result
-
-    else:
-        return _buffer_by_m(
-            line=line,
-            quad_segs=quad_segs,
-            cap_style=cap_style,
-            join_style=join_style,
-            mitre_limit=mitre_limit,
-        )
 
 
 def _buffer_by_m(
@@ -110,10 +112,12 @@ def _buffer_by_m(
     else:
         raise ValueError("Input geometry must have M or Z values for buffer distances.")
 
+    # Extract points and distances
     coords = shapely.get_coordinates(line, include_m=include_m, include_z=include_z)
-
     pts = shapely.points(coords[:, :2])
     distances = coords[:, 2]
+
+    # Buffer each point by its M/Z value
     buffers = shapely.buffer(
         pts,
         distances,
@@ -122,11 +126,21 @@ def _buffer_by_m(
         join_style=join_style,
         mitre_limit=mitre_limit,
     )
+
+    # Zero-distance points get empty geometries, so replace those with the original
+    # points. Negative distances also result in empty geometries, but we don't want to
+    # recuperate those.
+    zero_m_indices = np.argwhere(distances == 0)
+    buffers[zero_m_indices] = pts[zero_m_indices]
+
+    # Create convex hulls between each pair of buffers
     hull_inputs = [
         MultiPoint(shapely.get_coordinates([buffer1, buffer2]))
         for buffer1, buffer2 in pairwise(buffers)
     ]
     hulls = shapely.convex_hull(hull_inputs)
+
+    # Union all hulls to get the final buffer
     buffer_geom = shapely.union_all(hulls)
 
     if buffer_geom.is_empty:
--- tests/test_buffer_by_m.py
+++ tests/test_buffer_by_m.py
@@ -1,21 +1,47 @@
-from shapely.geometry import LineString, MultiPolygon
+import pytest
+
+from shapely.geometry import LineString, Polygon, MultiPolygon
 
 from pygeoops._buffer_by_m import buffer_by_m
+import test_helper
 
 
-def test_buffer_by_m():
-    line_coords = [
-        [0, 0, 3],
-        [5, -2, 4],
-        [10, 2, 0],
-        [15, 0, 2],
-        [20, 5, 5],
-        [25, 0, 2],
-        [35, 0, 2],
-    ]
+@pytest.mark.parametrize(
+    "line_coords, exp_type, exp_parts_relation",
+    [
+        ([[0, 6, 1], [0, 0, 2], [10, 0, 2], [13, 5, 4]], Polygon, None),
+        ([[0, 6, 1], [0, 0, 0], [10, 0, 2], [13, 5, 4]], MultiPolygon, "touches"),
+        ([[0, 6, 1], [0, 0, -1], [10, 0, 2], [13, 5, 4]], MultiPolygon, "disjoint"),
+    ],
+)
+def test_buffer_by_m(tmp_path, line_coords, exp_type, exp_parts_relation):
+    """Test buffer_by_m function.
+
+    Specific cases tested:
+    - a normal case with M values all > 0 will produce a single Polygon
+    - a case with one M value = 0 will produce a MultiPolygon with touching parts, as
+      the point(s) with zero distance will be represented as the original point
+    - a case with one M value < 0 will produce a MultiPolygon with disjoint parts, as
+      the point(s) with negative distance will be omitted from the buffer
+    """
     line = LineString(line_coords)
 
     buffer_geom = buffer_by_m(line)
 
-    assert isinstance(buffer_geom, MultiPolygon)
+    # Check result
+    assert isinstance(buffer_geom, exp_type)
     assert not buffer_geom.is_empty
+    if exp_parts_relation is not None:
+        parts = list(buffer_geom.geoms)
+        if exp_parts_relation == "disjoint":
+            assert len(parts) == 2
+            assert parts[0].disjoint(parts[1])
+        elif exp_parts_relation == "touches":
+            assert len(parts) == 2
+            assert parts[0].touches(parts[1])
+        else:
+            raise ValueError(f"Unknown expected parts relation: {exp_parts_relation}")
+
+    # Plot for visual inspection
+    output_path = tmp_path / "test_buffer_by_m.png"
+    test_helper.plot([buffer_geom, line], output_path)
